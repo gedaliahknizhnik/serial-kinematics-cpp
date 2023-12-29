2d85534ee007aeca260e9c82ab5c9bd54b368f4f
Gedaliah Knizhnik <gedaliah.knizhnik@gmail.com>
2023-12-29 10:01:51 -0500

    Added prismatic joint unit tests

--- tests/kinematics.cpp
+++ tests/kinematics.cpp
@@ -17,4 +17,17 @@ TEST_CASE("Test homogeneous transforms", "[Kinematics]") {
         1;
     REQUIRE(H2.isApprox(kinematics::get_transform(joint, 1.32), 1e-3));
   }
+
+  SECTION("Prismatic joint") {
+    kinematics::Joint joint{kinematics::Type::prismatic, M_PI / 2, 0.6, 0.8,
+                            M_PI / 6};
+
+    Eigen::Matrix4d H1;
+    H1 << 0, -0.866, 0.5, 0, 1, 0, 0, 0.6, 0, 0.5, 0.866, 0.8, 0, 0, 0, 1;
+    REQUIRE(H1.isApprox(kinematics::get_transform(joint, 0), 1e-3));
+
+    Eigen::Matrix4d H2;
+    H2 << 0, -0.866, 0.5, 0, 1, 0, 0, 0.6, 0, 0.5, 0.866, 1.8, 0, 0, 0, 1;
+    REQUIRE(H2.isApprox(kinematics::get_transform(joint, 1), 1e-3));
+  }
 }
